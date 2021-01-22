14181da719cbede5edb26154325e3a1edc6da5af
raghu5910 <raghuram5910@gmail.com>
2021-01-22 09:55:17 +0530

    na

--- project-data-warehouse-with-redshift/create_tables.py
+++ project-data-warehouse-with-redshift/create_tables.py
@@ -4,6 +4,9 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drop tables in Redshift database using the queries in `drop_table_queries` list.
+    """
     for query in drop_table_queries:
         print(query)
         cur.execute(query)
@@ -11,6 +14,9 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
+    """
+    Create tables in Redshift database using the queries in `create_table_queries` list.
+    """
     for query in create_table_queries:
         print(query)
         cur.execute(query)
@@ -18,6 +24,16 @@ def create_tables(cur, conn):
 
 
 def main():
+    """
+    - Establishes connection with the sparkify database and gets
+    cursor to it.
+
+    - Drops all the tables.
+
+    - Creates all tables needed.
+
+    - Finally, closes the connection.
+    """
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
--- project-data-warehouse-with-redshift/etl.py
+++ project-data-warehouse-with-redshift/etl.py
@@ -4,24 +4,42 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 buckets into staging tables
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Query data from staging tables and insert them
+    into corresponding dimension and fact tables.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Establishes connection with the sparkify database and gets
+    cursor to it.
+    - Loads data from S3 bucket into staging tables.
+    - Inserts data in dimensional and fact tables.
+    - Finally, closes the connection.
+    """
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
