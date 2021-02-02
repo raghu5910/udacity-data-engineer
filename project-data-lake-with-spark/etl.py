b9e90ecbf9233bce4455384fc31ffc52216bb5af
raghu5910 <raghuram5910@gmail.com>
2021-02-02 16:03:16 +0530

    commented code

--- project-data-lake-with-spark/etl.py
+++ project-data-lake-with-spark/etl.py
@@ -30,6 +30,9 @@ os.environ["AWS_SECRET_ACCESS_KEY"] = config["AWS_SECRET_ACCESS_KEY"]
 
 
 def create_spark_session():
+    """
+    return a spark session object
+    """
     spark = SparkSession.builder.config(
         "spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0"
     ).getOrCreate()
@@ -37,6 +40,11 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    Read data from S3 bucket and process song data to
+    song, artist dimension tables and write them as parquet
+    files into a S3 bucket
+    """
     song_data = input_data + "song_data/*/*/*/*"
 
     song_schema = StructType(
@@ -80,6 +88,11 @@ FROM songs_stage
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+    Read data from S3 bucket and process log data to
+    users, time dimension tables and songplays fact table
+    - write them as parquet files into a S3 bucket
+    """
     log_data = input_data + "log_data/*/*/*"
 
     event_schema = StructType(
